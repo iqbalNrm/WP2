de8846097eb05b46bae77a8c7819b1bbe4b46033
iqbalNrm <muhiqbal.95621@gmail.com>
2023-10-13 16:22:46 +0700

    mengganti file

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -5,7 +5,7 @@ class Web extends CI_Controller{
 
     function __construct(){ 
         parent::__construct(); 
-        $this->load->helper('url'); 
+         
    } 
         public function index(){
             $data['judul'] = "Halaman Depan";
@@ -14,4 +14,12 @@ class Web extends CI_Controller{
             $this->load->view('v_footer',$data);
             
         }
+        public function about()
+        {
+                $data['judul'] = "Halaman About";
+                $this->load->view('v_header', $data);
+                $this->load->view('v_about', $data);
+                $this->load->view('v_footer', $data);
+        }
 }
+        
